c8dea2063eb21bb90b50eb641f863a1f2eb59f3c
frjlnsyh <frjlnsyh@gmail.com>
2024-12-03 22:31:20 +0700

    Penambahan README.md dan juga perubahan sedikit pada game_angka.js

---
+++ README.md
@@ -0,0 +1,25 @@
+ini adalah pembelajaran Javascript dari Web Programing Unpas yang sudah mencapai closure
+
+pertama itu belajar tentang
+<li>Looping/Perulangan Bersarang</li>
+
+Lalu menyelesaikan pembelajaran Array
+<li>Program Array</li>
+
+Lalu mencoba membuat game dengan pengkodisan IF ELSE lalu menggunakan MATH.random untuk menentukan nilai random
+
+lalu mencoba membuat game dengan variable berisi array dengan MATH.floor berfungsis untuk menggenapkan MATH.random * 30
+<li>Prompt <sub>Inputan/memasukan</sub></li>
+<li>alert <sub>yang memunculkan Pop Up/Notifkasi</sub></li>
+yang didalamnya ada IF ELSE dan juga perulangan for dengan tambahan (PUSH)
+
+lalu memperdalam fungi yang sudah di pelajari
+<li>Literasi</li>
+<li>Deklarasi</li>
+<li>Objek</li>
+
+lalu belajar bagaimana fungsi atau javascript meng=eksekusi code nya 
+
+lalu memperdalam Object.create yang berujung menggunakan Prototype
+<li>Object.create()</li>
+<li>Prototype</li>
--- game_angka.js
+++ game_angka.js
@@ -1,52 +1,52 @@
-// var benar = true;
-// while (benar) {
+var benar = true;
+while (benar) {
 
-//     var p = prompt('Pilih Angka 1 - 30 \nKesempatan anda hanya 3x');
+    var p = prompt('Pilih Angka 1 - 30 \nKesempatan anda hanya 3x');
 
-//     var jawab = Math.floor(Math.random() * 30);
+    var jawab = Math.floor(Math.random() * 30);
 
-//     console.log(jawab);
+    console.log(jawab);
 
-//     hasil = '';
+    hasil = '';
 
-//     if (p == jawab) {
-//         hasil = 'Kamu menebak angkanya';
-//     } else if (p === '') {
-//         hasil = 'Kamu mengisi kosong'
-//     } else {
-//         for (var i = 3; i > 0; i--) {
-//             // If Open
-//             if (p < jawab) {
-//                 (p === '') ? i = 0 : p = prompt('Angkanya terlalu Kecil \nkesempatan kamu tinggal ' + i);
+    if (p == jawab) {
+        hasil = 'Kamu menebak angkanya';
+    } else if (p === '') {
+        hasil = 'Kamu mengisi kosong'
+    } else {
+        for (var i = 3; i > 0; i--) {
+            // If Open
+            if (p < jawab) {
+                (p === '') ? i = 0 : p = prompt('Angkanya terlalu Kecil \nkesempatan kamu tinggal ' + i);
 
-//                 (p == jawab) ? hasil = 'Kamu benar di jawaban ke - ' + i : hasil = 'Kamu salah dalam 3x percobaan';
-//             } else if (p > jawab) {
-//                 (p === '') ? i = 0 : p = prompt('Angkanya terlalu Besar \nkesempatan kamu tinggal ' + i);
+                (p == jawab) ? hasil = 'Kamu benar di jawaban ke - ' + i : hasil = 'Kamu salah dalam 3x percobaan';
+            } else if (p > jawab) {
+                (p === '') ? i = 0 : p = prompt('Angkanya terlalu Besar \nkesempatan kamu tinggal ' + i);
 
-//                 (p == jawab) ? hasil = 'Kamu benar di jawaban ke - ' + i : hasil = 'Kamu salah dalam 3x percobaan';
-//             }
-//             // If Close
-//         }
-//     }
-
-//     alert(hasil)
-//     benar = confirm('Main Lagi ?');
-// }
-
-// alert('Terimakasih :)')
-
-function loop(n) {
-    for (var i = 1; i < 6; i++) {
-        hasil = n * i;
-        console.log(hasil);
+                (p == jawab) ? hasil = 'Kamu benar di jawaban ke - ' + i : hasil = 'Kamu salah dalam 3x percobaan';
+            }
+            // If Close
+        }
     }
+
+    alert(hasil)
+    benar = confirm('Main Lagi ?');
 }
-loop(1);
 
-var buttonClick = (x, z) => (x ** 3) + (z ** 3);
+alert('Terimakasih :)')
 
-console.log(buttonClick(2, 3)); // Output: 35
+// function loop(n) {
+//     for (var i = 1; i < 6; i++) {
+//         hasil = n * i;
+//         console.log(hasil);
+//     }
+// }
+// loop(1);
 
-function buttonClisk(x, z) {
+// var buttonClick = (x, z) => (x ** 3) + (z ** 3);
+
+// console.log(buttonClick(2, 3)); // Output: 35
+
+// function buttonClisk(x, z) {
     
-}
+// }
